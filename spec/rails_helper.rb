85008144328dbe51a91aa4b8627d3325a54529e1
Blake Johnson <blake41@gmail.com>
2016-02-05 11:25:10 -0500

    needed to do some odd things to get the specs working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,9 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+RSpec.configure do |c|
+  c.include Features::SessionHelpers
+end
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
