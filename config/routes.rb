c259edd9d9656ac0616d0a28294e59cff9199b95
coltonstaab1 <colton.staab@gmail.com>
2016-03-04 21:32:45 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+	get '/' => 'welcome#home'
+	get '/about' => 'welcome#about'
+
+	root 'welcome#home'
 end
