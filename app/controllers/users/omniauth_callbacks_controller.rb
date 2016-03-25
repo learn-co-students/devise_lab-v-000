1d0274c8cc273c15260ea42727cd830a33a504c5
zachnewburgh <zach.newburgh@gmail.com>
2016-03-25 18:18:38 -0400

    Done.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # TODO
-  end
-
-  def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user      
   end
