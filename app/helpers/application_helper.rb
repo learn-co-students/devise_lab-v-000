a2d1e5fe381417a0c8bbbb2deb56f03e11decbc1
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-06 18:22:23 +0000

    adds sign in/sign out helper to application layout file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def sign_in_sign_out
+    content_tag(:div) do 
+      if user_signed_in?
+        link_to('Sign out', destroy_user_session_path, method: :delete) if user_signed_in?
+      else
+        link_to('Login', new_user_session_path)
+      end
+    end
+  end
 end
