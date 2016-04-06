ade7301b441451c159c86df2e6027b371bedaba8
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-06 21:34:30 +0000

    adds navigation links and styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   # devise_for :users
   get 'welcome/home'
-
+  get '/about', to: 'welcome#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
