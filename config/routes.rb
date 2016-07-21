eb33af8cfab676bc60c6a6f5ae43569b3ff7c871
SiamKing <thesiameseelectric@gmail.com>
2016-07-21 15:28:23 -0700

    devise a plan to take down the man

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'welcome#home'
+  get '/', to: 'welcome#home'
+  get '/about', to: 'welcome#about'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
