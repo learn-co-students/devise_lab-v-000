b361e1ba59488016191da331f6a09063ff6c563f
kaileeagray <kaileeagray@gmail.com>
2016-08-19 14:40:47 -0500

    Done.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def home
   end
+  def about
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, "558031261061637", "63f0c74640873c340438f0531e3c40d3"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
