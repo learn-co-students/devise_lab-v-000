82e078e3171d5bd7e3272efcbc332fa52a4c18c9
kaileeagray <kaileeagray@gmail.com>
2016-08-19 14:47:29 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'welcome#home'
   get '/about' => 'welcome#about'
   # The priority is based upon order of creation: first created -> highest priority.
