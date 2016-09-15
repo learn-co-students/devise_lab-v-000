19dc69255b6e7b6eea35fe9f64c44a79660f0d8f
Han Lee <hanlee12@gmail.com>
2016-09-15 20:31:19 +0000

    3 F's out of 16

--- Gemfile
+++ Gemfile
@@ -54,4 +54,5 @@ group :test do
   gem 'database_cleaner'
   gem 'launchy'
   gem 'selenium-webdriver'
+  gem 'pry'
 end
--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Feature: Sign out
 #   As a user
 #   I want to sign out
@@ -13,9 +14,8 @@ feature 'Sign out', :devise do
     signin(user.email, user.password)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
     click_link 'Sign out'
+  #  binding.pry
     expect(page).to have_content I18n.t 'devise.sessions.signed_out'
   end
 
 end
-
-
