8fb296036b60ad720f423ea23d5a96cd6c9a6d37
jbok4 <jbok4@aol.com>
2016-09-18 16:27:10 -0400

    adjust password length parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  devise :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable,
+          :omniauthable, :omniauth_providers => [:facebook]
 
 
   def self.from_omniauth(auth)
