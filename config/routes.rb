35c14eb4d912d9c324863932f0b84798a92452b3
Yechiel Kalmenson <chilikkal@gmail.com>
2016-10-16 05:52:51 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#home'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'about' => 'welcome#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
