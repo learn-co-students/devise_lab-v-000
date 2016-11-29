5aee03951174efd9db43ffb1b3c09edc70463296
Ivan Pilot <ivanpilot@gmail.com>
2016-11-29 00:48:19 +0000

    still under progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
 
+  # def self.from_omniauth(auth)
+  #   self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0,20]
+  #   end
+  # end
+
   def self.from_omniauth(auth)
-    self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'welcome#home'
   # devise_for :users
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
-
+  get '/about' => 'welcome#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
