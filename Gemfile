b6aa3606a6f279e1a2b1d45281c250b59a413606
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-29 20:32:43 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'high_voltage'
+gem 'omniauth-facebook'
 
 group :development do
   gem 'better_errors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/home' => 'welcome#home'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
