ee481c9a00f88b3cfee8ac63a720e6b004c2b4ba
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-12-14 17:30:23 -0800

    Done.

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,14 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect after_sign_in_path_for(@user)
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+
+  end
+end
