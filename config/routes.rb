4429c20c6db4b3a3e4c12b37ea824e9dbf5af746
github username <github email address>
2017-05-15 18:49:27 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
  # devise_for :users
   root 'welcome#home'
-  devise_for :users 
-  # :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/about', to: 'static#about'
 end
