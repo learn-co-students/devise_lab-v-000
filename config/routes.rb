c2cfe3f430d0f9edf8f20c8d71c71dda62753651
Bryan Hou <bryan.hou94@gmail.com>
2017-06-08 14:13:34 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
   get '/about' => 'welcome#about'
   root 'welcome#home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
