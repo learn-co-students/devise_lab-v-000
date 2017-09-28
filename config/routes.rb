3e4c2fa471fe0f85dd7cf46356f3e7ff5dabe7aa
Aimee Carney <acarney29@gmail.com>
2017-09-28 00:59:47 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'welcome/home'
+  get '/about', to: 'welcome#about'
+  root 'welcome#home'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
