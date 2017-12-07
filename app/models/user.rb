aa5d0819aa182084f4d18a34e0c62e39f2b447f5
github username <github email address>
2017-12-06 20:42:36 -0500

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
+  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :validatable, password_length: 8..12
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+end
