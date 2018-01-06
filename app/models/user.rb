187000ab3960c8e9c6cdeaabfa38713f61667f35
bkhartmire <bkhartmire@gmail.com>
2018-01-05 21:25:22 -0800

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  validates :email, presence: true
+  validates :password, presence: true
+
+  def self.from_omniatuh(auth)
+    binding.pry
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
