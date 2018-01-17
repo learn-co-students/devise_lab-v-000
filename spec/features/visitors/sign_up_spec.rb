f9e2682e271479f7ce1c37024bcc01c787a7c392
github username <github email address>
2018-01-17 07:30:59 -0500

    Done.

--- spec/features/visitors/sign_up_spec.rb
+++ spec/features/visitors/sign_up_spec.rb
@@ -36,6 +36,8 @@ feature 'Sign Up', :devise do
   #   Given I am not signed in
   #   When I sign up with a short password
   #   Then I see a 'too short password' message
+
+  ##########
   scenario 'visitor cannot sign up with a short password' do
     sign_up_with('test@example.com', 'please', 'please')
     expect(page).to have_content "Password is too short"
