9d23ff69e03d1621bdb1a12d33f788edf2610a8a
Eric McKinney <mckinney99@gmail.com>
2018-02-06 21:28:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"
+ }
+ root 'welcome#home'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+ get '/about' => 'welcome#about'
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
