88cd33f99409669a484e76df290fba4e77a63154
Justin Tooley <jstooley823@gmail.com>
2018-02-06 23:58:11 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  #To take care of error when running test. Devise tests for 6 character minimum, while in spec/features/visitors/sign_up_spec.rb, the test parameter is 6 characters (please). As a result, test does not pass.
+  validates :password, length: {minimum: 7}
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
