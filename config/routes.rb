86975cc2d4045055d96856c89910ba7962239f58
Hannah Carnes <hecarnes@gmail.com>
2018-02-06 20:59:57 -0500

    use home action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "welcome#index"
+  devise_for :users
+  root to: "welcome#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
