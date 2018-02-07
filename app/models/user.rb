8a9d4a5ececa3c54a4e7493fbd0fdc7b1dca8644
Hannah Carnes <hecarnes@gmail.com>
2018-02-06 21:54:49 -0500

    done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :password, :length => {:within => 8..40}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
