c8562772274991928f1c1a9bfd4a8440454e3857
dwyn <dwayne.harmon@gmail.com>
2018-02-14 13:51:36 -0600

    Ouu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root "welcome#home"
 
   get '/about' => 'welcome#about'
 end
-
-# , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
