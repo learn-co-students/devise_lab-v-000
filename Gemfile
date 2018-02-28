bb242a37bb851f0086e2b9f85962828b29b9b780
github username <github email address>
2018-02-28 18:35:27 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -41,7 +41,7 @@ group :development do
 end
 
 group :development, :test do
-  gem 'factory_girl_rails'
+  gem 'factory_bot_rails'
   gem 'faker'
   gem 'rspec-rails'
 end
--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -13,7 +13,6 @@ feature 'Sign in', :devise do
   scenario 'user cannot sign in if not registered' do
 
     signin('test@example.com', 'please123')
-    binding.pry
     expect(page).to have_content(/#{I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')}/i)
   end
 
